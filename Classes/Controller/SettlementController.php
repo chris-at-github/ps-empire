61e1b6d9db83d562068e95d15456ad3355e31030
chris@kubuntu <pschorr.christian@gmail.com>
2019-04-23 22:59:24 +0200

    2019-04-23@0.0.1

--- Classes/Controller/SettlementController.php
+++ Classes/Controller/SettlementController.php
@@ -12,8 +12,6 @@ class SettlementController extends ApiController {
 	 * @return void
 	 */
 	public function listingAction() {
-//		DebuggerUtility::var_dump($this->objectManager->get(\Ps\Empire\Domain\Repository\GameObjectRepository::class)->findAll());
-		$settlement = $this->objectManager->get(\Ps\Empire\Factory\Settlement::class)->create('Ps\Empire\Object\Settlement\Colony', ['title' => 'Settlement SC01']);
-		DebuggerUtility::var_dump($settlement);
+		DebuggerUtility::var_dump($this->objectManager->get(\Ps\Empire\Manager\Settlement::class)->find());
 	}
 }
--- Classes/Manager/Settlement.php
+++ Classes/Manager/Settlement.php
@@ -2,13 +2,41 @@
 
 namespace Ps\Empire\Manager;
 
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Extbase\Annotation as Extbase;
+use Ps\Empire\Domain\Repository\GameObjectRepository;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
-class Settlement {
+class Settlement implements SingletonInterface {
+
+	/**
+	 * @Extbase\Inject
+	 * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @Extbase\Inject
+	 * @var \Ps\Empire\Domain\Repository\GameObjectRepository
+	 */
+	protected $gameObjectRepository;
 
 	/**
 	 * @param array $options
-	 * @return void
+	 * @return array
 	 */
 	public function find($options = []) {
+		$settlements = [];
+
+		foreach($this->gameObjectRepository->findAll() as $model) {
+
+			/** @var \Ps\Empire\Domain\Model\GameObject $model */
+			/** @var \Ps\Empire\Object\Settlement\Settlement $settlement */
+			$settlement = $this->objectManager->get(\Ps\Empire\Factory\Settlement::class)->create(
+				$model->getFqcn(), json_decode($model->getProperties(), true)
+			);
+
+			DebuggerUtility::var_dump($settlement);
+		}
 	}
 }
